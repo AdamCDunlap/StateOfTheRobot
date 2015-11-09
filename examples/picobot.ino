00f40dca3584093581eafbb834563a9348f3e43a
Adam Dunlap <dunlapadam@gmail.com>
2015-11-08 23:49:12 -0800

    Making each state its own function

--- examples/picobot.ino
+++ examples/picobot.ino
@@ -27,38 +27,43 @@ void initialize() {
     pinMode(13, OUTPUT);
 }
 
-void main_loop() {
-    if (state == Start) {
-        bot.move(0, 1);
-        if (bot.senseDir(Dir::EAST)) {
-            set_state(GoNorth);
-        } else if (tm_in_state().ms() > 10000) {
-            set_state(Confused);
-        }
-    } else if (state == GoNorth) {
-        bot.move(1, 0);
-        if (bot.senseDir(Dir::NORTH)) {
-            set_state(GoSouth);
-        } else if (tm_in_state().ms() > 10000) {
-            set_state(Confused);
-        }
-    } else if (state == GoSouth) {
-        bot.move(-1, 0);
-        if (bot.senseDir(Dir::SOUTH)) {
-            set_state(GoNorth);
-        } else if (tm_in_state().ms() > 10000) {
-            set_state(Confused);
-        }
-    } else if (state == Confused) {
-        Serial.println("I'm confused! I got here from state " + last_state());
-        digitalWrite(13, LOW);
-        wait(500_ms);
-        digitalWrite(13, HIGH);
-        wait(500_ms);
-    } else if (state == Panic) {
-        every(100_ms) {
-             Serial.println("BEAR!");
-        }
+void main_Start() {
+    bot.move(0, 1);
+    if (bot.senseDir(Dir::EAST)) {
+        set_state(GoNorth);
+    } else if (tm_in_state().ms() > 10000) {
+        set_state(Confused);
+    }
+}
+
+void main_GoNorth() {
+    bot.move(1, 0);
+    if (bot.senseDir(Dir::NORTH)) {
+        set_state(GoSouth);
+    } else if (tm_in_state().ms() > 10000) {
+        set_state(Confused);
+}
+
+void main_GoSouth() {
+    bot.move(-1, 0);
+    if (bot.senseDir(Dir::SOUTH)) {
+        set_state(GoNorth);
+    } else if (tm_in_state().ms() > 10000) {
+        set_state(Confused);
+    }
+}
+
+void main_Confused() {
+    Serial.println("I'm confused! I got here from state " + last_state());
+    digitalWrite(13, LOW);
+    wait(500_ms);
+    digitalWrite(13, HIGH);
+    wait(500_ms);
+}
+
+void main_Panic() {
+    every(100_ms) {
+         Serial.println("BEAR!");
     }
 }
 
